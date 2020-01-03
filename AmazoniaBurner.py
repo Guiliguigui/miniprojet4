a673fe42e6e03ff94acd8ca5b8e4204d3b599cab
Guillaume <gui.mairesse@gmail.com>
2020-01-03 20:57:45 +0100

    Bugfix

--- AmazoniaBurner.py
+++ AmazoniaBurner.py
@@ -8,28 +8,28 @@ from tkinter import *
 import argparse
 import random
 
-def afficherCarte(carte,ids):
-    for row in range(len(carte)):
-        for col in range(len(carte[row])):
-            if(carte[row][col]=='a'):
+def showMap():
+    for row in range(rows):
+        for col in range(cols):
+            if(data[row][col]=='a'):
                 ids[row][col] = canvas.create_rectangle(col * cell_size,
                                                         row * cell_size,
                                                         (col + 1) * cell_size,
                                                         (row + 1) * cell_size,
                                                         fill="green")
-            elif(carte[row][col]=='f'):
+            elif(data[row][col]=='f'):
                 ids[row][col] = canvas.create_rectangle(col * cell_size,
                                                         row * cell_size,
                                                         (col + 1) * cell_size,
                                                         (row + 1) * cell_size,
                                                         fill="red")
-            elif(carte[row][col]=='c'):
+            elif(data[row][col]=='c'):
                 ids[row][col] = canvas.create_rectangle(col * cell_size,
                                                         row * cell_size,
                                                         (col + 1) * cell_size,
                                                         (row + 1) * cell_size,
                                                         fill="grey")
-            elif(carte[row][col]=='v'):
+            elif(data[row][col]=='v'):
                 ids[row][col] = canvas.create_rectangle(col * cell_size,
                                                         row * cell_size,
                                                         (col + 1) * cell_size,
@@ -39,13 +39,44 @@ def afficherCarte(carte,ids):
 def click_callback(event):
     row = int(event.y / cell_size)
     col = int(event.x / cell_size)
-    if(carte[row][col]=='a'):
-        carte[row][col] = 'f'
+    if(data[row][col]=='a'):
+        data[row][col] = 'f'
         canvas.itemconfig(ids[row][col], fill="red")
-    elif(carte[row][col]=='f'):
-        carte[row][col] = 'a'
+    elif(data[row][col]=='f'):
+        data[row][col] = 'a'
         canvas.itemconfig(ids[row][col], fill="green")
 
+def launch():
+    future_data = [['v' for i in range(cols)] for i in range(rows)]
+    for row in range(rows):
+        for col in range(cols):
+            if(data[row][col]=='a'):
+                k = 0
+                if(row!=rows-1 and data[row+1][col]=='f'):
+                    k+=1
+                if(row!=0 and data[row-1][col]=='f'):
+                    k+=1
+                if(col!=cols-1 and data[row][col+1]=='f'):
+                    k+=1
+                if(col!=0 and data[row][col-1]=='f'):
+                    k+=1
+                if(rule==1 and k>=1):
+                    future_data[row][col] = 'f'
+                elif(rule==2 and random.random()<(1-1/(k+1))):
+                    future_data[row][col] = 'f'
+                else:
+                    future_data[row][col] = 'a'
+            elif(data[row][col]=='f'):
+                future_data[row][col] = 'c'
+            elif(data[row][col]=='c'):
+                future_data[row][col] = 'v'
+            elif(data[row][col]=='v'):
+                future_data[row][col] = 'v'
+    for row in range(rows):
+        for col in range(cols):
+            data[row][col] = future_data[row][col]
+    showMap()
+    root.after(int(duration*1000),launch)
 
 
 if __name__=='__main__':
@@ -56,11 +87,12 @@ if __name__=='__main__':
     parser.add_argument("-duration", help="to choose the duration of the animation (default is 2)",default=2)
     parser.add_argument("-cell_size", help="to choose the size of the cells (default is 35)",default=35)
     parser.add_argument("-afforestation", help="to choose the percentage of afforestation (default is 0.6)",default=.6)
-    parser.add_argument("-rule", help="to choose the rule (default is 1)",default=1, choices=[1,2,])
+    parser.add_argument("-rule", help="to choose the rule (default is 1)",default=1, choices=['1','2',])
     args=parser.parse_args()
 
     rows = int(args.rows)
     cols = int(args.cols)
+    duration= float(args.duration)
     cell_size = int(args.cell_size)
     afforestation = float(args.afforestation)
     rule = int(args.rule)
@@ -72,16 +104,19 @@ if __name__=='__main__':
     canvas_width = cols * cell_size
     canvas_height = rows * cell_size
     canvas = Canvas(root, width = canvas_width, height = canvas_height)
-    canvas.pack()
+    canvas.grid(column=0, row=0)
 
-    carte = [['v' for i in range(10)] for i in range(10)]
-    ids = [[0 for i in range(10)] for i in range(10)]
+    btnLaunch = Button(root, text = "Launch", command = launch)
+    btnLaunch.grid(column=0,row=1)
+
+    data = [['v' for i in range(cols)] for i in range(rows)]
+    ids = [[0 for i in range(cols)] for i in range(rows)]
     for row in range(rows):
         for col in range(cols):
             if(random.random()<afforestation):
-                carte[row][col] = 'a'
+                data[row][col] = 'a'
             
-    afficherCarte(carte,ids)
+    showMap()
 
     canvas.bind('<Button-1>',click_callback)
 
